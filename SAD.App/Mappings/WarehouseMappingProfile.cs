cadcde2e165b438c3e8770cbdad546c1d38a8164
Grzegorz Tomaszewski <tomasgr_aehit@students.vizja.pl>
2024-02-18 19:26:35 +0100

    Koniec, Przejście do mediatora -Command -Query

--- SAD.App/Mappings/WarehouseMappingProfile.cs
+++ SAD.App/Mappings/WarehouseMappingProfile.cs
@@ -9,13 +9,15 @@ namespace SAD.App.Mappings
     public class WarehouseMappingProfile : Profile
     {
         public WarehouseMappingProfile()
-        {
+        {   // map z formularza dto do encji
+            // automaper rozpozna jeśli są  te same nazwy i typy
             CreateMap<WarehouseDto, Domain.Entities.Warehouse>().ForMember(e => e.PalletRack, opt => opt.MapFrom(src => new PalletRack()
             {
                 Name = src.PalletRackName,
                 Position = src.PalletRackPosition,
             }));
-
+            // map z encji do dto
+            // automaper rozpozna jeśli są  te same nazwy i typy
             CreateMap<Domain.Entities.Warehouse, WarehouseDto>()
                 .ForMember(wd => wd.PalletRackName, opt => opt.MapFrom(src => src.PalletRack.Name))
                 .ForMember(wd => wd.PalletRackPosition, opt => opt.MapFrom(src => src.PalletRack.Position));
